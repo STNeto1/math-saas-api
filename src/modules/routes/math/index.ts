72cf02b282713690c502fd71080b41295fc2935b
STNeto1 <gfreitasneto18@gmail.com>
2022-11-30 12:25:49 -0300

    math operations

--- src/modules/routes/math/index.ts
+++ src/modules/routes/math/index.ts
@@ -4,7 +4,7 @@ import {
 } from 'fastify'
 import fp from 'fastify-plugin'
 import zodToJsonSchema from 'zod-to-json-schema'
-import { sumResponseSchema, TTwoNumbersSchema, twoNumbersSchema } from './schemas'
+import { divisionResponseSchema, multiplicationResponseSchema, subtractionResponseSchema, sumResponseSchema, TTwoNumbersSchema, TTwoNumbersWithGtZeroSchema, twoNumbersSchema, twoNumbersWithGtZero } from './schemas'
 
 
 export default fp(
@@ -25,6 +25,66 @@ export default fp(
 
         return reply.status(200).send({
           sum: body.first + body.second
+        })
+      }
+    })
+
+    server.route({
+      url: '/math/subtraction',
+      logLevel: 'warn',
+      onRequest: [server.checkToken],
+      method: ['POST'],
+      schema: {
+        body: zodToJsonSchema(twoNumbersSchema, 'twoNumbersSchema'),
+        response: {
+          200: zodToJsonSchema(subtractionResponseSchema, 'subtractionResponseSchema')
+        }
+      },
+      handler: async (request, reply) => {
+        const body = request.body as TTwoNumbersSchema
+
+        return reply.status(200).send({
+          subtraction: body.first - body.second
+        })
+      }
+    })
+
+    server.route({
+      url: '/math/multiplication',
+      logLevel: 'warn',
+      onRequest: [server.checkToken],
+      method: ['POST'],
+      schema: {
+        body: zodToJsonSchema(twoNumbersSchema, 'twoNumbersSchema'),
+        response: {
+          200: zodToJsonSchema(multiplicationResponseSchema, 'multiplicationResponseSchema')
+        }
+      },
+      handler: async (request, reply) => {
+        const body = request.body as TTwoNumbersSchema
+
+        return reply.status(200).send({
+          multiplication: body.first * body.second
+        })
+      }
+    })
+
+    server.route({
+      url: '/math/division',
+      logLevel: 'warn',
+      onRequest: [server.checkToken],
+      method: ['POST'],
+      schema: {
+        body: zodToJsonSchema(twoNumbersWithGtZero, 'twoNumbersWithGtZero'),
+        response: {
+          200: zodToJsonSchema(divisionResponseSchema, 'divisionResponseSchema')
+        }
+      },
+      handler: async (request, reply) => {
+        const body = request.body as TTwoNumbersWithGtZeroSchema
+
+        return reply.status(200).send({
+          division: body.first / body.second
         })
       }
     })
--- src/modules/routes/math/schemas.ts
+++ src/modules/routes/math/schemas.ts
@@ -6,7 +6,28 @@ export const twoNumbersSchema = z.object({
   second: z.number()
 })
 
+export type TTwoNumbersWithGtZeroSchema = z.infer<typeof twoNumbersWithGtZero>
+export const twoNumbersWithGtZero = z.object({
+  first: z.number(),
+  second: z.number().gt(0)
+})
+
 export type TSumResponseSchema = z.infer<typeof sumResponseSchema>
 export const sumResponseSchema = z.object({
   sum: z.number()
+})
+
+export type TSubtractionResponseSchema = z.infer<typeof subtractionResponseSchema>
+export const subtractionResponseSchema = z.object({
+  subtraction: z.number()
+})
+
+export type TMultiplicationResponseSchema = z.infer<typeof multiplicationResponseSchema>
+export const multiplicationResponseSchema = z.object({
+  multiplication: z.number()
+})
+
+export type TDivisionResponseSchema = z.infer<typeof divisionResponseSchema>
+export const divisionResponseSchema = z.object({
+  division: z.number()
 })
